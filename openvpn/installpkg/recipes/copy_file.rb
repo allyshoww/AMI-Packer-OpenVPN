a838902b966918b8b0b51e740f90490c79845d5c
Allyson Oliveira <asoliveira@outlook.com>
2019-04-02 12:28:41 -0300

    Deleting manual tests

--- openvpn/installpkg/recipes/copy_file.rb
+++
@@ -1,7 +0,0 @@
-cookbook_file "Copy a file" do  
-  group "root"
-  mode "0755"
-  owner "ec2-user"
-  path "/home/allyson/chef-cookbook/openvpn/installpkg/recipes/openvpn-spec.rb"
-  source "openvpn-spec.rb"  
-end
